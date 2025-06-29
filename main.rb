d72a91dbdf27d03e0d4e20aeabdb2a2d9c35ff75
Justin Bento <44421842+Justin-Bento@users.noreply.github.com>
2025-06-29 09:51:38 -0400

    Created a pseudo-code for basic layout.

--- main.rb
+++ main.rb
@@ -3,3 +3,13 @@
 # Program: Ruby Course
 # Date: 2025-06-29
 
+# Pseudo-Code 
+# Description → The Caeser's Cipher is an encryption technique that subsitiutes each letter in the text file by a letter some fixed number of positions down the alphabet. So a shift of 4 it will move four down, example A will become D.
+
+# → [] Create your own method called caesar_cipher.
+# → [] Apply two paramaters called message, and shifter.
+  # → [] The first method 'message' returns a string outpt.
+  # → [] The second method 'shifter' returns a number outpt.
+# → [] Ensure the text and the message output both at the same time.
+  # → [] Keep the text output the same casing.
+ 
