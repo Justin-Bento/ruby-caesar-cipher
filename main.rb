c565dbad01d0e0475a09a7b781ecb6ffd2b8c7a4
Justin Bento <44421842+Justin-Bento@users.noreply.github.com.>
2025-07-02 10:06:12 -0400

    added a way to shift the values.

--- main.rb
+++ main.rb
@@ -2,7 +2,7 @@ message = "JavaScript, Sucks"
 
 def caesar_cipher(plain_text, shift = 3)
   ascii_code = plain_text.chars.map.with_index { |string| string.ord }.join
-  
+  shifted = ascii_code.map { |char| char+shift }
 end 
 
 puts caesar_cipher(message)
