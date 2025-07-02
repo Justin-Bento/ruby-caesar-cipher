9a8b56b72702fa7594e62e873384303cb9cc222e
Justin Bento <44421842+Justin-Bento@users.noreply.github.com.>
2025-07-02 10:03:26 -0400

    Changed upcase to order to convert the order to ASCII code.

--- main.rb
+++ main.rb
@@ -1,7 +1,7 @@
 message = "JavaScript, Sucks"
 
 def caesar_cipher(plain_text, shift = 3)
-  plain_text.chars.map.with_index { |string| string.upcase }.join
+  plain_text.chars.map.with_index { |string| string.ord }.join
 end 
 
 puts caesar_cipher(message)
