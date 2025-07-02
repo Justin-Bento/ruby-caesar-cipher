79cb6451bcf2afc360174fc9fdd9dc49e58f77b4
Justin Bento <44421842+Justin-Bento@users.noreply.github.com.>
2025-07-02 09:59:19 -0400

    Added join ruby method to convert each element of the array to a string on output

--- main.rb
+++ main.rb
@@ -1,7 +1,7 @@
 message = "JavaScript, Sucks"
 
 def caesar_cipher(plain_text, shift = 3)
-  plain_text.chars.map.with_index { |string| string.upcase }
+  plain_text.chars.map.with_index { |string| string.upcase }.join
 end 
 
 puts caesar_cipher(message)
