208c48f7246e733577418a10f87c76cada718ba7
Justin Bento <44421842+Justin-Bento@users.noreply.github.com.>
2025-07-02 10:07:02 -0400

    convert and join our new values in the shifted array into a string of new characters associated with our ASCII codes

--- main.rb
+++ main.rb
@@ -3,6 +3,7 @@ message = "JavaScript, Sucks"
 def caesar_cipher(plain_text, shift = 3)
   ascii_code = plain_text.chars.map.with_index { |string| string.ord }.join
   shifted = ascii_code.map { |char| char+shift }
+  shifted.map {|char| char.chr}.join
 end 
 
 puts caesar_cipher(message)
