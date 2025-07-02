2d1e5801aff121873daa5b8b81890ab8eab9b3a8
Justin Bento <44421842+Justin-Bento@users.noreply.github.com.>
2025-07-02 10:05:12 -0400

    Moved ascii code into a variable called 'ascii_code'

--- main.rb
+++ main.rb
@@ -1,7 +1,8 @@
 message = "JavaScript, Sucks"
 
 def caesar_cipher(plain_text, shift = 3)
-  plain_text.chars.map.with_index { |string| string.ord }.join
+  ascii_code = plain_text.chars.map.with_index { |string| string.ord }.join
+  
 end 
 
 puts caesar_cipher(message)
