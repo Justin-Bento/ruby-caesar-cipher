c027af9060c1a3feb7b5de47acae01293a2615bf
Justin Bento <44421842+Justin-Bento@users.noreply.github.com>
2025-07-03 14:00:00 -0400

    Commit backdated to yesterday

--- main.rb
+++ main.rb
@@ -3,7 +3,7 @@ message = "JavaScript, Sucks"
 def caesar_cipher(plain_text, shift = 3)
   ascii_code = plain_text.chars.map.with_index { |string| string.ord }
   shifted = ascii_code.map { |char| char+shift }
-  shifted.map {|char| char.chr}.join
+  shifted.map {|char| char.chr.match(/\A[a-zA-Z'-]*\z/)}.join
 end 
 
 puts caesar_cipher(message)
