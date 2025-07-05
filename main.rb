403f71394890e7b652304af8790a7b0b481e2729
Justin Bento <44421842+Justin-Bento@users.noreply.github.com>
2025-07-05 15:20:09 -0400

    Removed default value for shift as that was supposed to be included in the final function.

--- main.rb
+++ main.rb
@@ -1,6 +1,6 @@
 message = "JavaScript, Sucks"
 
-def caesar_cipher(plain_text, shift = 3)
+def caesar_cipher(plain_text, shift)
   ascii_code = plain_text.chars.map.with_index { |string| string.ord }
   shifted = ascii_code.map { |char| char+shift }
   shifted.map {|char| char.chr.match(/\A[a-zA-Z'-]*\z/)}.join
@@ -9,5 +9,5 @@ end
 if message.empty? == true
   puts "please, say something."
 else 
-  puts caesar_cipher(message)
+  puts caesar_cipher(message, 3)
 end
