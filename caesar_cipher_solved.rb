1393869cf1f884380ecd6249c696c04e36e72567
Justin Bento <44421842+Justin-Bento@users.noreply.github.com>
2025-07-07 12:41:51 -0400

    formatted document.

--- caesar_cipher_solved.rb
+++ caesar_cipher_solved.rb
@@ -1,27 +1,27 @@
 def caesar_cipher(plain_text, shift)
-  puts reset = shift % 26
-  shifted = plain_text.chars.map { |string|
-    if string.ord >= 65 && string.ord <= 90 ## Uppercase Letters
-      if string.ord+reset > 90
-        string.ord+reset - 26
-      else
-        string.ord+reset
-      end
-    elsif string.ord >= 97 && string.ord <= 122 ## Logic Uppercase Letters
-    if string.ord+reset > 122
-      string.ord+reset - 26
-    else
-      string.ord+reset
-    end
-    else
-      string.ord
-    end
-  }
-  shifted.map {|char| char.chr}.join
+	puts reset = shift % 26
+	shifted = plain_text.chars.map { |string|
+		if string.ord >= 65 && string.ord <= 90 ## Uppercase Letters
+			if string.ord+reset > 90
+				string.ord+reset - 26
+			else
+				string.ord+reset
+			end
+		elsif string.ord >= 97 && string.ord <= 122 ## Logic Uppercase Letters
+			if string.ord+reset > 122
+				string.ord+reset - 26
+			else
+				string.ord+reset
+			end
+		else
+			string.ord
+		end
+	}
+	shifted.map {|char| char.chr}.join
 end
 
 if message.empty? == true
-  puts "please, say something."
+	puts "please, say something."
 else
-  puts caesar_cipher(message, 32)
+	puts caesar_cipher(message, 32)
 end
